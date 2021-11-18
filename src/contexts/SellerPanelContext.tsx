c056a52151e5fbd2244cf716a0a1a5b86df0d1c2
Kamil Wójcik <wojcikakamil@gmail.com>
2021-11-18 16:08:30 +0100

    added auto updating second navbar while scrolling

--- src/contexts/SellerPanelContext.tsx
+++ src/contexts/SellerPanelContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, PropsWithChildren, useState} from "react";
+import {createContext, PropsWithChildren, useEffect, useState} from "react";
 import {MessageDescriptor} from "react-intl";
 
 export interface SellerPanelPath {
@@ -11,14 +11,13 @@ class SellerPanelHolder {
     public activePath?: SellerPanelPath;
 
     constructor(paths?: SellerPanelPath[], activePath?: SellerPanelPath) {
-        this.paths = paths ? paths : [];
+        this.paths = paths !== undefined && paths !== null ? paths : [];
         this.activePath = activePath;
     }
 
+
     public clone(): SellerPanelHolder {
-        let clone = new SellerPanelHolder(this.paths, this.activePath);
-        console.log("Clone: ", clone.paths);
-        return clone;
+        return new SellerPanelHolder(this.paths.filter(p => p !== null && p !== undefined), this.activePath);
     }
 }
 
@@ -37,6 +36,7 @@ class SellerPanelContextHolder {
             this.holder.activePath = path;
         }
 
+        this.holder.paths = this.holder.paths.filter(p => p.elementId !== path.elementId);
         this.holder.paths.push(path);
         this.update();
     }
@@ -73,31 +73,65 @@ class SellerPanelContextHolder {
         this.holder = this.holder.clone();
         this.updateHolder(this.holder);
     }
+
 }
+
 
 export const SellerPanelContext = createContext<SellerPanelContextHolder>(new SellerPanelContextHolder(new SellerPanelHolder(), () => {
 }));
 
 export const SellerPanelProvider = (props: PropsWithChildren<any>) => {
     const [holder, updateHolder] = useState<SellerPanelHolder>(new SellerPanelHolder());
-    // useEffect(() => {
-    //     const listener = () => {
-    //         // console.log(window.screen.);
-    //     };
-    //     window.addEventListener('wheel', listener);
-    //
-    //     return () => {
-    //         window.removeEventListener('wheel', listener);
-    //     }
-    // }, [])
+    const holderState = {holder, updateHolder};
 
+    useEffect(() => {
+        const listener = () => {
+            if (holder.paths.length === 0) {
+                return;
+            }
+            let newActiveElId = smallestTopOffset(holder.paths.map(p => p.elementId));
+            if (newActiveElId !== holder.activePath?.elementId) {
+                updateHolder(new SellerPanelHolder(holder.paths,
+                    holder.paths.find(p => p.elementId === newActiveElId)));
+            }
+        };
+        window.addEventListener('wheel', listener);
+
+        return () => {
+            window.removeEventListener('wheel', listener);
+        }
+    }, [holder])
 
     return (
-        <SellerPanelContext.Provider value={new SellerPanelContextHolder(holder.clone(),
-            (val: any) => updateHolder(val))}>
+        <SellerPanelContext.Provider value={new SellerPanelContextHolder(holderState.holder, holderState.updateHolder)}>
             {
                 props.children
             }
         </SellerPanelContext.Provider>
     )
+}
+
+function smallestTopOffset(elIds: string[]): string {
+    let searchedId = "";
+    let smallestOffset = 10000;
+    for (let i = 0; i < elIds.length; i++) {
+        let id = elIds[i];
+        let el = document.getElementById(id);
+
+        if (el === null || el === undefined) {
+            continue;
+        }
+
+        let box = el.getBoundingClientRect();
+        if (searchedId === "") {
+            searchedId = id;
+            smallestOffset = Math.abs(box.top);
+        }
+        if (Math.abs(box.top) < smallestOffset) {
+            searchedId = id;
+            smallestOffset = Math.abs(box.top);
+        }
+    }
+
+    return searchedId;
 }
--- src/molecules/SellerPanelComponentRegister.tsx
+++ src/molecules/SellerPanelComponentRegister.tsx
@@ -5,12 +5,11 @@ import {MessageDescriptor} from "react-intl";
 const SellerPanelComponentRegister = (props: { message: MessageDescriptor, elementId: string }) => {
     const spContext = useContext(SellerPanelContext);
 
+
     useEffect(() => {
-        console.log("mount " + props.elementId);
         spContext.registerPath({message: props.message, elementId: props.elementId});
 
         return () => {
-            console.log("unmount " + props.elementId);
             spContext.unregisterPath(props.elementId);
         }
     }, [props.message, props.elementId]);
